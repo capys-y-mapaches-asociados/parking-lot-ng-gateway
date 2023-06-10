3e706d76fa040c548ab8e9c42b2097ac0ddc6765
zacwastaken <seamzacdisc1@gmail.com>
2023-06-09 22:52:37 -0500

    Initial version of gateway generated by generator-jhipster@7.9.3

---
+++ src/main/java/co/edu/icesi/papupros/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package co.edu.icesi.papupros.repository;
+
+import co.edu.icesi.papupros.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
---
+++ src/main/java/co/edu/icesi/papupros/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package co.edu.icesi.papupros.service.dto;
